86c753a38cc8409c34e74dfbb280cd9533089680
Greg Wiley <gwiley@cj.com>
2019-02-05 07:59:03 -0800

    graphiql page

--- src/App.js
+++ src/App.js
@@ -1,28 +1,24 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import GraphiQL from 'graphiql';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <GraphiQL fetcher = {graphQLFetcher}/>
       </div>
     );
   }
 }
 
 export default App;
+
+function graphQLFetcher(graphQLParams) {
+    return fetch("https://zd9mdqlr7d.execute-api.us-east-1.amazonaws.com/stage/graphql", {
+      method: 'post',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(graphQLParams),
+    }).then(response => response.json());
+  }
+
